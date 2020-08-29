a85efaa34516e66c555a4026a9665a8e14c8ccf3
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2020-08-29 21:22:52 +0200

    Update swift-tools version

--- Tests/CollectionViewKitTests/CollectionViewKitTests.swift
+++ Tests/CollectionViewKitTests/CollectionViewKitTests.swift
@@ -6,7 +6,7 @@ final class CollectionViewKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CollectionViewKit().text, "Hello, World!")
+        XCTAssertEqual(Section<String>(title: "Planets").title, "Hello, World!")
     }
 
     static var allTests = [
